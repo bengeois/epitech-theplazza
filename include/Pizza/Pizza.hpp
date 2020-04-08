a0f87bdad7639461a0fa272cb9d07974931c8731
Benjamin Bourgeois <benjishift@gmail.com>
2020-04-08 22:14:37 +0200

    Add Pizza Type and Pizza Size

---
+++ include/Pizza/Pizza.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2020
+** CPP_plazza_2019
+** File description:
+** TODO: add description
+*/
+
+#ifndef CPP_PLAZZA_2019_PIZZA_HPP
+#define CPP_PLAZZA_2019_PIZZA_HPP
+
+namespace Plazza
+{
+    enum PizzaType
+    {
+        Regina = 1,
+        Margarita = 2,
+        Americana = 4,
+        Fantasia = 8,
+    };
+
+    enum PizzaSize
+    {
+        S = 1,
+        M = 2,
+        L = 4,
+        XL = 8,
+        XXL = 16,
+    };
+}
+
+#endif //CPP_PLAZZA_2019_PIZZA_HPP
