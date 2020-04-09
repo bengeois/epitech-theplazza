ea29b5ffe1d93f9a142c56a1ede05d36eeeaee8f
Amaury <amaury.lecomte@epitech.eu>
2020-04-09 12:55:43 +0200

    Add IIPC interface, MessageQueue class and Order class

---
+++ include/IPC/IIPC.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** Epitech-ThePlazza
+** File description:
+** IIPC
+*/
+
+#ifndef IIPC_HPP_
+#define IIPC_HPP_
+
+class IIPC {
+    public:
+        IIPC();
+        ~IIPC();
+
+    protected:
+    private:
+};
+
+#endif /* !IIPC_HPP_ */
---
+++ include/Order/Order.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2020
+** Epitech-ThePlazza
+** File description:
+** Order
+*/
+
+#ifndef ORDER_HPP_
+#define ORDER_HPP_
+
+namespace Plazza
+{
+    class Order {
+        public:
+            Order();
+            ~Order();
+
+        private:
+    };
+} // namespace Plazza
+
+#endif /* !ORDER_HPP_ */
